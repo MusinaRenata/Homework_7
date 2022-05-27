7fffbdb3359208630aa1cb1523b3e003f1de06eb
Renata <myska2@yandex.ru>
2022-05-27 08:08:33 +0300

    t7.1

---
+++ Task_7.1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Спирально заполнить двумерный массив
+
+int[,] spiral = new int[5, 5];
+int m = spiral.GetLength(0);
+int n = spiral.GetLength(1);
+int s = 1;
+
+for (int j = 0; j < n; j++)
+{
+    spiral[0, j] = s;
+    s++;
+}
+
+for (int i = 1; i < m; i++)
+{
+    spiral[i, n - 1] = s;
+    s++;
+}
+
+for (int j = n - 2; j >= 0; j--)
+{
+    spiral[m - 1, j] = s;
+    s++;
+}
+
+for (int i = m - 2; i > 0; i--)
+{
+    spiral[i, 0] = s;
+    s++;
+}
